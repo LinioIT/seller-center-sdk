6d7a89cdd09282a9ccb3eab4a7d291848d13bae9
Emiliano Castro <emilianocast98@gmail.com>
2020-08-19 16:11:39 -0300

    refactor: create base manager

--- src/Service/FeedManager.php
+++ src/Service/FeedManager.php
@@ -5,55 +5,17 @@ declare(strict_types=1);
 namespace Linio\SellerCenter\Service;
 
 use DateTimeInterface;
-use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Psr7\Request;
-use Linio\SellerCenter\Application\Configuration;
-use Linio\SellerCenter\Application\Parameters;
 use Linio\SellerCenter\Application\Security\Signature;
 use Linio\SellerCenter\Factory\Xml\Feed\FeedFactory;
 use Linio\SellerCenter\Factory\Xml\Feed\FeedsFactory;
 use Linio\SellerCenter\Formatter\LogMessageFormatter;
 use Linio\SellerCenter\Model\Feed\Feed;
 use Linio\SellerCenter\Response\HandleResponse;
-use Linio\SellerCenter\Response\SuccessResponse;
-use Psr\Http\Message\RequestInterface;
-use Psr\Log\LoggerInterface;
 
-class FeedManager
+class FeedManager extends BaseManager
 {
     private const FEED_OFFSET_LIST_ACTION = 'FeedOffsetList';
-
-    /**
-     * @var LoggerInterface
-     */
-    protected $logger;
-
-    /**
-     * @var Configuration
-     */
-    protected $configuration;
-
-    /**
-     * @var ClientInterface
-     */
-    protected $client;
-
-    /**
-     * @var Parameters
-     */
-    protected $parameters;
-
-    public function __construct(
-        Configuration $configuration,
-        ClientInterface $client,
-        Parameters $parameters,
-        LoggerInterface $logger
-    ) {
-        $this->configuration = $configuration;
-        $this->client = $client;
-        $this->parameters = $parameters;
-        $this->logger = $logger;
-    }
 
     public function getFeedStatusById(string $id): Feed
     {
@@ -200,11 +162,11 @@ class FeedManager
         $formattedUpdatedAfter = null;
 
         if ($createdAfter) {
-            $formattedCreatedAfter = $createdAfter->format('Y-m-d\TH:i:s');
+            $formattedCreatedAfter = $createdAfter->format(self::DATE_TIME_FORMAT);
         }
 
         if ($updatedAfter) {
-            $formattedCreatedAfter = $updatedAfter->format('Y-m-d\TH:i:s');
+            $formattedCreatedAfter = $updatedAfter->format(self::DATE_TIME_FORMAT);
         }
 
         $parameters->set([
@@ -215,7 +177,6 @@ class FeedManager
             'UpdatedDate' => $formattedCreatedAfter,
         ]);
 
-        $this->updateSignature($parameters);
         $requestId = $this->generateRequestId();
         $response = $this->executeAction($action, $parameters, $requestId);
         $list = FeedsFactory::make($response->getBody());
@@ -228,92 +189,5 @@ class FeedManager
         ));
 
         return array_values($list->all());
-    }
-
-    private function logRequest(
-        string $action,
-        string $requestId,
-        RequestInterface $request,
-        Parameters $parameters
-    ): void {
-        $this->logger->debug(
-            LogMessageFormatter::fromAction($requestId, $action, LogMessageFormatter::TYPE_REQUEST),
-            [
-                'url' => (string) $request->getUri(),
-                'method' => $request->getMethod(),
-                'body' => (string) $request->getBody(),
-                'parameters' => $parameters->all(),
-            ]
-        );
-    }
-
-    private function logRawResponse(string $action, string $requestId, string $body): void
-    {
-        $this->logger->debug(
-            LogMessageFormatter::fromAction($requestId, $action, LogMessageFormatter::TYPE_RESPONSE),
-            [
-                'body' => $body,
-            ]
-        );
-    }
-
-    private function logHandledResponse(string $action, string $requestId, SuccessResponse $handledResponse): void
-    {
-        $this->logger->debug(
-            LogMessageFormatter::fromAction($requestId, $action, LogMessageFormatter::TYPE_BUILT_RESPONSE),
-            [
-                'head' => $handledResponse->getHead()->asXML(),
-                'body' => $handledResponse->getBody()->asXML(),
-            ]
-        );
-    }
-
-    private function makeParametersForAction(string $actionName): Parameters
-    {
-        $parameters = clone $this->parameters;
-        $parameters->set([
-            'Action' => $actionName,
-        ]);
-
-        return $parameters;
-    }
-
-    private function generateRequestId(): string
-    {
-        return uniqid((string) mt_rand());
-    }
-
-    private function updateSignature(Parameters $parameters): void
-    {
-        $parameters->set([
-            'Signature' => Signature::generate(
-                $parameters,
-                $this->configuration->getKey()
-            )->get(),
-        ]);
-    }
-
-    private function executeAction(
-        string $action,
-        Parameters $parameters,
-        string $requestId
-    ): SuccessResponse {
-        $request = new Request('GET', $this->configuration->getEndpoint(), [
-            'Request-ID' => $requestId,
-        ]);
-
-        $this->logRequest($action, $requestId, $request, $parameters);
-
-        $response = $this->client->send($request, ['query' => $parameters->all()]);
-
-        $body = (string) $response->getBody();
-
-        $this->logRawResponse($action, $requestId, $body);
-
-        $builtResponse = HandleResponse::parse($body);
-
-        $this->logHandledResponse($action, $requestId, $builtResponse);
-
-        return $builtResponse;
     }
 }
---
+++ tests/Unit/Service/BaseManagerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Linio\SellerCenter\Unit\Service;
+
+use GuzzleHttp\ClientInterface;
+use Linio\SellerCenter\Application\Configuration;
+use Linio\SellerCenter\Application\Parameters;
+use Linio\SellerCenter\LinioTestCase;
+use Linio\SellerCenter\Service\BaseManager;
+use PHPUnit\Framework\MockObject\MockObject;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+class BaseManagerTest extends LinioTestCase
+{
+    /**
+     * @var ObjectProphecy
+     */
+    protected $configurationStub;
+
+    /**
+     * @var ObjectProphecy
+     */
+    protected $clientStub;
+
+    /**
+     * @var Parameters
+     */
+    protected $parametersStub;
+
+    /**
+     * @var ObjectProphecy
+     */
+    protected $loggerStub;
+
+    /**
+     * @var BaseManager|MockObject
+     */
+    private $baseManager;
+
+    public function setUp(): void
+    {
+        $this->configurationStub = new Configuration('foo', 'bar', 'baz');
+        $this->clientStub = $this->prophesize(ClientInterface::class);
+        $this->parametersStub = new Parameters();
+        $this->loggerStub = $this->prophesize(LoggerInterface::class);
+
+        $this->baseManager = $this->getMockForAbstractClass(BaseManager::class, [
+            $this->configurationStub,
+            $this->clientStub->reveal(),
+            $this->parametersStub,
+            $this->loggerStub->reveal(),
+        ]);
+    }
+
+    public function testItGeneratesRequestIds(): void
+    {
+        $requestId = $this->baseManager->generateRequestId();
+        $this->assertIsString($requestId);
+        $this->assertNotEmpty($requestId);
+    }
+
+    public function testItBuildQueryParameters(): void
+    {
+        $this->parametersStub->set(['foo' => true]);
+        $queryParameters = $this->baseManager->buildQuery($this->parametersStub);
+        $this->assertCount(2, $queryParameters);
+        $this->assertArrayHasKey('foo', $queryParameters);
+        $this->assertArrayHasKey('Signature', $queryParameters);
+    }
+
+    public function testItExecutesAction(): void
+    {
+        $response = $this->prophesize(ResponseInterface::class);
+        $response->getBody()->shouldBeCalled()->willReturn($this->getRawSuccessResponse());
+
+        $this->loggerStub
+            ->debug(Argument::type('string'), Argument::type('array'))
+            ->shouldBeCalledTimes(3);
+
+        $this->clientStub
+            ->send(
+                Argument::type(RequestInterface::class),
+                Argument::type('array')
+            )
+            ->shouldBeCalled()
+            ->willReturn($response->reveal());
+
+        $this->baseManager->executeAction('FooAction', $this->parametersStub, '');
+        $this->assertTrue(true);
+    }
+
+    private function getRawSuccessResponse(): string
+    {
+        return '<?xml version="1.0" encoding="UTF-8"?>
+                    <SuccessResponse>
+                         <Head>
+                         </Head>
+                         <Body>
+                         </Body>
+                    </SuccessResponse>';
+    }
+}
