034a064430e68594d3c50ad84622c0b9e72cc776
miguel <miguel@serfe.com>
2021-06-10 16:45:54 -0300

    feat: add new attribute sales type to order item and refactor tests and factories

--- src/Factory/Xml/Order/AddressFactory.php
+++ src/Factory/Xml/Order/AddressFactory.php
@@ -6,54 +6,31 @@ namespace Linio\SellerCenter\Factory\Xml\Order;
 
 use Linio\SellerCenter\Exception\InvalidXmlStructureException;
 use Linio\SellerCenter\Model\Order\Address;
+use Linio\SellerCenter\Validator\XmlStructureValidator;
 use SimpleXMLElement;
 
 class AddressFactory
 {
+    private const XML_MODEL = 'Address';
+    private const REQUIRED_FIELDS = [
+        'FirstName',
+        'LastName',
+        'Phone',
+        'Phone2',
+        'CustomerEmail',
+        'City',
+        'Ward',
+        'Region',
+        'PostCode',
+        'Country',
+    ];
+
     public static function make(SimpleXMLElement $element): Address
     {
-        if (!property_exists($element, 'FirstName')) {
-            throw new InvalidXmlStructureException('Address', 'FirstName');
-        }
-
-        if (!property_exists($element, 'LastName')) {
-            throw new InvalidXmlStructureException('Address', 'LastName');
-        }
-
-        if (!property_exists($element, 'Phone')) {
-            throw new InvalidXmlStructureException('Address', 'Phone');
-        }
-
-        if (!property_exists($element, 'Phone2')) {
-            throw new InvalidXmlStructureException('Address', 'Phone2');
-        }
+        XmlStructureValidator::validateStructure($element, self::XML_MODEL, self::REQUIRED_FIELDS);
 
         if (!property_exists($element, 'Address1')) {
             throw new InvalidXmlStructureException('Address', 'Address');
-        }
-
-        if (!property_exists($element, 'CustomerEmail')) {
-            throw new InvalidXmlStructureException('Address', 'CustomerEmail');
-        }
-
-        if (!property_exists($element, 'City')) {
-            throw new InvalidXmlStructureException('Address', 'City');
-        }
-
-        if (!property_exists($element, 'Ward')) {
-            throw new InvalidXmlStructureException('Address', 'Ward');
-        }
-
-        if (!property_exists($element, 'Region')) {
-            throw new InvalidXmlStructureException('Address', 'Region');
-        }
-
-        if (!property_exists($element, 'PostCode')) {
-            throw new InvalidXmlStructureException('Address', 'PostCode');
-        }
-
-        if (!property_exists($element, 'Country')) {
-            throw new InvalidXmlStructureException('Address', 'Country');
         }
 
         return new Address(
