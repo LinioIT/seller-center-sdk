3747957daacf944c7ebb242222aa955b78f28af1
mramosp <mramosp@falabella.cl>
2026-06-02 14:30:26 -0400

    fix: rename and separate methods

--- src/Transformer/Product/ProductTransformer.php
+++ src/Transformer/Product/ProductTransformer.php
@@ -57,7 +57,7 @@ class ProductTransformer
             }
 
             if (strtolower((string) $attributeKey) === 'description') {
-                $value = self::removeTagsFromHtmlText((string) $attributeValue);
+                $value = self::sanitizeHtmlAndWrapInCData((string) $attributeValue);
                 $productData->addChild((string) $attributeKey, $value);
                 continue;
             }
@@ -66,7 +66,14 @@ class ProductTransformer
         }
     }
 
-    public static function removeTagsFromHtmlText(string $html): string
+    public static function sanitizeHtmlAndWrapInCData(string $html): string
+    {
+        $cleanText = self::removeUnwantedHtmlTags($html);
+
+        return self::wrapInCData($cleanText);
+    }
+
+    public static function removeUnwantedHtmlTags(string $html): string
     {
         $decodedText = html_entity_decode($html);
 
@@ -82,14 +89,16 @@ class ProductTransformer
             'a' => '/<a\b[^>]*>.*?<\/a>/i',
         ];
 
-        $processedText = trim((string) preg_replace($regexFilters, '', $decodedText));
-        $processedText = trim((string) preg_replace('/[\x{1F1E6}-\x{1F1FF}\x{1F300}-\x{1F5FF}\x{1F600}-\x{1F64F}\x{1F680}-\x{1F6FF}\x{1F700}-\x{1F77F}\x{1F780}-\x{1F7FF}\x{1F800}-\x{1F8FF}\x{1F900}-\x{1F9FF}\x{1FA00}-\x{1FAFF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{FE0F}\x{200D}\x{20E3}\x{1F3FB}-\x{1F3FF}]/u', '', $processedText));
+        return trim((string) preg_replace($regexFilters, '', $decodedText));
+    }
 
-        if (empty($processedText)) {
+    public static function wrapInCData(string $content): string
+    {
+        if ($content === '') {
             return '';
         }
 
-        return sprintf('<![CDATA[%s]]>', $processedText);
+        return sprintf('<![CDATA[%s]]>', $content);
     }
 
     /**
